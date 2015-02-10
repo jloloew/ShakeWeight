c45385b859c01e3cc8d7272f00740b459f819ba1
Justin Loew <jloloew@gmail.com>
2015-02-10 16:38:10 -0600

    started on the ViewController

--- ShakeWeight/ViewController.swift
+++ ShakeWeight/ViewController.swift
@@ -7,18 +7,55 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
 	@IBOutlet weak var reps: UILabel!
+	var healthStore: HKHealthStore? = nil
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		// health data is not available on iPad
+		if HKHealthStore.isHealthDataAvailable() {
+			healthStore = HKHealthStore()
+			// check whether we're authorized to save workout data
+			let authStatus = healthStore!.authorizationStatusForType(HKObjectType.workoutType())
+			switch authStatus {
+			case .SharingAuthorized:
+				loadHealthData()
+				
+			case .SharingDenied:
+				// nothing we can do about it
+				// we don't have permission, so the HKHealthStore is useless to us
+				self.healthStore = nil
+				
+			case .NotDetermined:
+				// prompt the user for permission
+				let types = NSSet(object: HKObjectType.workoutType())
+				healthStore?.requestAuthorizationToShareTypes(types, readTypes: types, completion: { (success: Bool, error: NSError!) -> Void in
+					// success is whether the user answered the prompt, *not* whether permission has been granted.
+					if error != nil {
+						println(error)
+						return // return from the block, not from viewDidLoad
+					} else {
+						// make sure we're actually authorized now
+						if self.healthStore!.authorizationStatusForType(HKObjectType.workoutType()) != .SharingAuthorized {
+							// we don't have permission, so the HKHealthStore is useless to us
+							self.healthStore = nil
+						} else {
+							// we're authorized!
+							self.loadHealthData()
+						}
+					}
+				})
+			}
+		}
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	// Load the data from previous workouts, if any
+	func loadHealthData() {
+		
 	}
 
 
